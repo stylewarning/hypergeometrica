b3dc1dc73576a3a087a05b3f4a4577060dd7706d
Robert Smith <robert@stylewarning.com>
2019-08-26 00:02:30 -0700

    allow MPZ-* to have varargs

--- src/ntt-multiply.lisp
+++ src/ntt-multiply.lisp
@@ -8,53 +8,63 @@
   (make-array n :element-type 'alexandria:non-negative-fixnum
                 :initial-element 0))
 
-(defun mpz-* (a b)
-  (let* ((size-a (mpz-size a))
-         (size-b (mpz-size b))
-         (result-size (+ 1 size-a size-b))
-         (result-storage (make-storage result-size))
-         (length (least-power-of-two->= result-size))
-         (a-ntt (make-ntt-storage length))
-         (b-ntt (make-ntt-storage length))
-         (m (first (find-suitable-moduli (max length (expt $base 2)))))
-         (w (ordered-root-from-primitive-root
-             (find-primitive-root m)
-             length
-             m)))
-    ;; Fill out the storage.
-    (map-into a-ntt #'identity (storage a))
-    (map-into b-ntt #'identity (storage b))
+(defun mpz-* (&rest xs)
+  (declare (dynamic-extent xs))
+  (cond
+    ((null xs)       (integer-mpz 1))
+    ((null (cdr xs)) (car xs))
+    ;; We are guaranteed at least two elements here.
+    (t
+     (let* ((num-factors (length xs))
+            (result-size (+ (1- num-factors)
+                            (reduce #'+ xs :key #'mpz-size)))
+            (result-storage (make-storage result-size))
+            (length (least-power-of-two->= result-size))
+            (result-ntt (make-ntt-storage length))
+            (temp-ntt (make-ntt-storage length))
+            ;; XXX: Can we avoid having to do BASE^N?
+            (m (first (find-suitable-moduli (max length (expt $base num-factors)))))
+            (w (ordered-root-from-primitive-root
+                (find-primitive-root m)
+                length
+                m)))
+       ;; Copy one of the factors and transform it
+       (replace result-ntt (storage (car xs)))
+       (setf result-ntt (ntt-forward result-ntt :modulus m :primitive-root w))
 
-    ;; Compute the forward transforms.
-    (setf a-ntt (ntt-forward a-ntt :modulus m :primitive-root w))
-    (setf b-ntt (ntt-forward b-ntt :modulus m :primitive-root w))
+       ;; Multiply in the other factors one at a time.
+       (dolist (x (cdr xs))
+         ;; Clear
+         (fill temp-ntt 0)
+         ;; Copy
+         (replace temp-ntt (storage x))
+         ;; Transform
+         (setf temp-ntt (ntt-forward temp-ntt :modulus m :primitive-root w))
+         ;; Pointwise multiply
+         (dotimes (i length)
+           (setf (aref result-ntt i)
+                 (m* (aref result-ntt i) (aref temp-ntt i) m))))
 
-    ;; Pointwise multiply into A-NTT
-    (dotimes (i length)
-      (setf (aref a-ntt i)
-            (m* (aref a-ntt i) (aref b-ntt i) m)))
+       ;; Inverse transform
+       (setf result-ntt (ntt-reverse result-ntt :modulus m :primitive-root w))
 
-    ;; Inverse transform
-    (setf a-ntt (ntt-reverse a-ntt :modulus m :primitive-root w))
+       ;; Unpack the result.
+       (loop :with carry := 0
+             :for i :below result-size
+             :for ci := (+ carry (aref result-ntt i))
+             :if (>= ci $base)
+               :do (multiple-value-setq (carry ci) (floor ci $base))
+             :else
+               :do (setf carry 0)
+             :do (setf (aref result-storage i) ci)
+             :finally (assert (zerop carry))
+                      (return (make-instance 'mpz :sign (reduce #'* xs :key #'sign)
+                                                  :storage result-storage)))))))
 
-    ;; Unpack the result.
-    (loop :with carry := 0
-          :for i :below result-size
-          :for ci := (+ carry (aref a-ntt i))
-          :if (>= ci $base)
-            :do (multiple-value-setq (carry ci) (floor ci $base))
-          :else
-            :do (setf carry 0)
-          :do (setf (aref result-storage i) ci)
-          :finally (assert (zerop carry))
-                   (return (make-instance 'mpz :sign (* (sign a) (sign b))
-                                               :storage result-storage)))))
-
-(defun test-* (m n)
-  (let* ((a (integer-mpz m))
-         (b (integer-mpz n))
-         (c (mpz-* a b)))
-    (format t "actual: ~D~%" (* m n))
-    (format t "ntt   : ~D~%" (mpz-integer c))
-    (format t "same: ~A~%" (= (* m n) (mpz-integer c)))
-    c))
+(defun test-* (&rest xs)
+  (let* ((p (apply #'mpz-* (mapcar #'integer-mpz xs)))
+         (q (apply #'* xs)))
+    (format t "actual: ~D~%" q)
+    (format t "ntt   : ~D~%" (mpz-integer p))
+    (format t "same: ~A~%" (= (mpz-integer p) q))
+    p))

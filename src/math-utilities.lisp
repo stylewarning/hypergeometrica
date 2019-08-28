c3a060682b73b319e0c379d4e56debd04b2890c3
Robert Smith <robert@stylewarning.com>
2019-08-28 00:05:26 -0700

    fix POWER-OF-TWO-P

--- src/math-utilities.lisp
+++ src/math-utilities.lisp
@@ -8,7 +8,8 @@
 
 (defun power-of-two-p (n)
   "Is N a power-of-two?"
-  (zerop (logand n (1- n))))
+  (and (plusp n)
+       (zerop (logand n (1- n)))))
 
 (defun next-power-of-two (n)
   "Find the minimum K such that N <= 2^K."
